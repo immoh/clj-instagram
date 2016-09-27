5b3629789fa9a6473a82572aa451113e508926a9
Immo Heikkinen <immo.heikkinen@gmail.com>
2016-09-27 07:08:22 +0300

    Bump dependencies

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "Clojure client for Instagram API"
   :url "https://github.com/immoh/clj-instagram"
   :source-paths ["src"]
-  :dependencies [[clj-http "2.2.0"]
+  :dependencies [[clj-http "3.3.0"]
                  [cheshire "5.6.3"]
-                 [buddy/buddy-core "0.13.0"]]
+                 [buddy/buddy-core "1.0.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [org.clojure/test.check "0.9.0"]
                                   [midje "1.8.3"]]
-                   :plugins [[lein-midje "3.2"]
-                             [lein-codox "0.9.5"]]}}
+                   :plugins [[lein-midje "3.2.1"]
+                             [lein-codox "0.10.0"]]}}
   :codox {:project {:name "clj-instagram"}
           :source-uri "https://github.com/immoh/clj-instagram/blob/{version}/{filepath}#L{line}"})
