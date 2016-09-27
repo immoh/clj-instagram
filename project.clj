cfd703ed74463a3b93fd40ea63f819c7218230e5
Immo Heikkinen <immo.heikkinen@gmail.com>
2016-09-27 07:10:49 +0300

    Next development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-instagram "0.2.0"
+(defproject clj-instagram "0.3.0-SNAPSHOT"
   :description "Clojure client for Instagram API"
   :url "https://github.com/immoh/clj-instagram"
   :source-paths ["src"]
